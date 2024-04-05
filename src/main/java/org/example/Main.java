c6561f1b6b8229e69ca444b8042252dabb6c200e
6ejlko1 <153452519+6ejlko1@users.noreply.github.com>
2024-04-05 10:34:43 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int Plus = calc.plus.apply(8, 2);
+        int Minus = calc.minus.apply(6, 6);
+        int Devide = calc.devide.apply(10, 2);
+        int Multyply = calc.multiply.apply(2, 100);
+        int Pow = calc.pow.apply(6);
+        int Abs = calc.abs.apply(10);
+
+        calc.println.accept(Plus);
+        calc.println.accept(Minus);
+        calc.println.accept(Multyply);
+        calc.println.accept(Pow);
+        calc.println.accept(Abs);
+
+        if (Devide == 0) {
+            throw new IllegalArgumentException("На 0 делить нельзя");
+        }
+        else calc.println.accept(Devide);
+    }
+}
---
+++ src/test/java/org.example/calcTest.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.Assert.assertEquals;
+
+public class calcTest {
+    Calculator calculator = new Calculator();
+    @Test
+    public void testPlus() {
+
+        int result = calculator.instance.get().plus.apply(3, 5);
+        Assertions.assertEquals(8, result);
+    }
+
+    @Test
+    public void testMinus() {
+        int result = calculator.instance.get().minus.apply(3, 5);
+        //assertEquals(-2, result);
+        Assertions.assertEquals(-2, result);
+    }
+
+    @Test
+    public void testDevide() {
+        int result = calculator.instance.get().devide.apply(10, 5);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    public void testDevideByZero() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.instance.get().devide.apply(5, 0);
+        });
+    }
+    @Test
+    public void testMultyply() {
+        int result = calculator.instance.get().multiply.apply(3, 5);
+        Assertions.assertEquals(15, result);
+    }
+    @Test
+    public void testAbs() {
+        int result = calculator.instance.get().abs.apply(-3);
+        Assertions.assertEquals(3, result);
+    }
+    @Test
+    public void testPow() {
+        int result = calculator.instance.get().pow.apply(5);
+        Assertions.assertEquals(25, result);
+    }
+}
+
